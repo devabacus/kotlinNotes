98aa2ae209f8c6d96290f8a1230c770ff5275445
devabacusnrf <devabacusnrf@gmail.com>
2019-03-26 08:55:45 +0500

    add model

---
+++ app/src/main/java/com/example/kotlinnotes/Model.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinnotes
+
+data class Task(
+    var title: String,
+    val todo: List<Todo>? = mutableListOf(),
+    var tag: Tag? = null
+){
+}
+
+
+data class Todo(
+    var description: String,
+    var isComplete: Boolean
+)
+
+data class Note(
+    var description: String,
+    var tag: Tag? = null
+)
+
+data class Tag(
+    val name: String,
+    val colourResId: Int
+)
+
