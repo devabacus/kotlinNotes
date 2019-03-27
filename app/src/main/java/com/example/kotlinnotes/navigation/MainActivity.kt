ac60c9ae5c1416caebb37dd89a639e2f2b34dd4f
devabacusnrf <devabacusnrf@gmail.com>
2019-03-27 05:00:46 +0500

    added two fragments before recyclerview

--- app/src/main/java/com/example/kotlinnotes/navigation/MainActivity.kt
+++ app/src/main/java/com/example/kotlinnotes/navigation/MainActivity.kt
@@ -6,20 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.kotlinnotes.R
 import com.example.kotlinnotes.notes.NotesListFragment
+import com.example.kotlinnotes.tasks.TaskListFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
+            R.id.tasks_item -> {
+                replaceFragment(TaskListFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
+            R.id.notes_item -> {
                 replaceFragment(NotesListFragment.newInstance())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -29,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(TaskListFragment.newInstance())
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
