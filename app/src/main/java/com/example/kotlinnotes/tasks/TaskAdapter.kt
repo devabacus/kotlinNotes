514dd8c390d0c02d917aed637e742ce5c3577612
scale-driver <devabacusnrf@gmail.com>
2019-03-28 08:25:59 +0500

    create custom view on java

--- app/src/main/java/com/example/kotlinnotes/tasks/TaskAdapter.kt
+++ app/src/main/java/com/example/kotlinnotes/tasks/TaskAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinnotes.R
 import com.example.kotlinnotes.foundations.BaseRecyclerAdapter
 import com.example.kotlinnotes.models.Task
+import com.example.kotlinnotes.views.TodoView
 import kotlinx.android.synthetic.main.item_task.view.*
 import kotlinx.android.synthetic.main.view_todo.view.*
 
@@ -20,20 +21,8 @@ class TaskAdapter(
             view.tv_task_item.text = data.title
 
             data.todo.forEach { todo->
-                val todoView = LayoutInflater.from(view.context).inflate(R.layout.view_todo, view.todo_container, false).apply {
-                    tv_description.text = todo.description
-                    cb_complete.isChecked = todo.isComplete
-                    if (todo.isComplete) {
-                        tv_description.paintFlags = tv_description.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                    }
-                    cb_complete.setOnCheckedChangeListener{buttonView, isChecked ->
-                        if (isChecked) {
-                            tv_description.paintFlags = tv_description.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                        } else {
-                            tv_description.paintFlags = tv_description.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
-                        }
-                    }
-
+                val todoView = (LayoutInflater.from(view.context).inflate(R.layout.view_todo, view.todo_container, false) as TodoView).apply {
+                    initView(todo)
                 }
                 view.todo_container.addView(todoView)
 
